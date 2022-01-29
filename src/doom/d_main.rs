967bcb97ea2cf4ecf435e61420b9b483605270f2
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-01-29 14:34:57 -0600

    Setup main structure based off of PrBoom

--- src/doom/d_main.rs
+++ src/doom/d_main.rs
@@ -1,23 +1,21 @@
-
-
 pub fn L_SetupConsoleMasks() {
-    let (mut p, mut i) = (0,0);
+    let (mut p, mut i) = (0, 0);
     let &mut cena = &mut "ICWEFDA";
     let mut pos = 0;
-    
-
 }
 
 pub fn D_DoomMainSetup() {
-    let (mut p, mut slot) = (0,0);
+    let (mut p, mut slot) = (0, 0);
 
-
+    L_SetupConsoleMasks();
 }
+
+pub fn D_DoomLoop() {}
 
 /*
  *  Main Function
  * */
 pub fn D_DoomMain() {
     D_DoomMainSetup();
-
+    D_DoomLoop();
 }
