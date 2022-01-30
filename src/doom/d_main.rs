2754185d7224c27e39efe8f808072434bce97841
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-01-30 15:42:41 -0600

    Misc changes

--- src/doom/d_main.rs
+++ src/doom/d_main.rs
@@ -1,6 +1,27 @@
 extern crate libc;
 use super::m_argv;
 
+static mut nomonsters: bool = false;
+static mut fastparm: bool = false;
+static mut respawnparm: bool = false;
+
+static standard_iwads: &'static [&'static str] = &[
+    "doom2f.wad",
+    "doom2.wad",
+    "plutonia.wad",
+    "tnt.wad",
+    "doom.wad",
+    "doom1.wad",
+    "doomu.wad",
+    "freedoom2.wad",
+    "freedoom1.wad",
+    "freedm.wad",
+    "hacx.wad",
+    "chex.wad",
+    "rekkrsa.wad",
+    "bfgdoom2.wad",
+    "bfgdoom.wad",
+];
 
 pub fn D_DoomMainSetup() {
     let (mut p, mut slot) = (0, 0);
@@ -11,7 +32,7 @@ pub fn D_DoomMainSetup() {
         rsp_found = false;
         for arg in m_argv::myargv() {
             let chr = arg.as_str().chars().nth(0).unwrap();
-            if (chr == '@') {
+            if chr == '@' {
                 println!("Found");
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 #[allow(non_snake_case)]
+#[allow(non_upper_case_globals)]
+#[allow(unused_mut)]
+#[allow(unused_variables)]
+#[allow(dead_code)]
+
 mod doom;
 
 fn main() {
